0d6d48cabcebf5cae20c4efc329645f2a0f66540
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2020-12-17 15:58:34 +0100

    Accordion - Use State

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Accordion from './components/accordion';
+
+const items = [
+  {
+    title: 'What is React',
+    content: 'React is a front end Javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use react by creatin components'
+  }
+]
+
+const App = () => {
+  return (
+    <div>
+      <Accordion items={items} />
+      </div>
+  )
+}
+
+export default App;
