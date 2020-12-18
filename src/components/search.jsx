ac224e16e016f567834037f234bc51a955a0e1f2
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2020-12-18 09:08:17 +0100

    Widget search wiki finished

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -2,30 +2,41 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);  
   const [results, setresults] = useState([]);
 
   useEffect(() => {
-    // const search = async () => {
-    //   const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-    //     params: {
-    //       action: 'query',
-    //       list: 'search',
-    //       origin: '*',
-    //       format: 'json',
-    //       srsearch: term
-    //     }
-    //   });
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 500)
 
-    //   setresults(data.query.search);
-    // };
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [term])
 
-    // const timeoutId = setTimeout(() => {
-    //   if (term){
-    //     search();
-    //   }
-    // }, 500)
-  }, [term]);
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm
+        }
+      });
+
+      setresults(data.query.search);
+    };
+
+    if (debouncedTerm) {
+      search();
+    }
+
+
+  }, [debouncedTerm])
 
   const renderResult = results.map((result) => {
     return (
